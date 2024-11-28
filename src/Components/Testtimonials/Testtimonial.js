006e51a82549dcb53a954079c276f556ecb304da
Precious1324 <aleriwaprecious70@gmail.com>
2024-11-28 15:46:06 +0100

    New Changes Made

--- src/Components/Testtimonials/Testtimonial.js
+++ src/Components/Testtimonials/Testtimonial.js
@@ -5,6 +5,17 @@ const Testtimonial = () => {
     return ( 
         <div className="testimony">
             <h3 className="testimony-header">Testimonials</h3>
+            <div className="testimonial-videos">
+                <iframe
+                    src="https://www.youtube.com/embed/JbFfWNG8zAw?si=o0j-WKKhANupxhV8&amp;start=10" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerpolicy="strict-origin-when-cross-origin" 
+                    allowfullscreen
+                >    
+                </iframe>
+            </div>
             <div className="testimony-body">
                 {testimonydata.map((testimony)=>{
                     return(
@@ -19,6 +30,17 @@ const Testtimonial = () => {
                         </div>
                     )
                 })}
+            </div>
+            <div className="testimonial-videos">
+                <iframe
+                    src="https://www.youtube.com/embed/7HubN8c-9-A?si=lhKpRht_az7DPxwk" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerpolicy="strict-origin-when-cross-origin" 
+                    allowfullscreen
+                >    
+                </iframe>
             </div>
         </div>
     );
